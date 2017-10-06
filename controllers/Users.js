6a34def67be588ebeea1bc2470a69605f39cc315
Aria Groult <aria.groult@outlook.fr>
2017-10-06 14:26:51 +0200

    added users on login module

---
+++ controllers/Users.js
@@ -0,0 +1,15 @@
+const Users = {
+    userList: [{
+            "mail": "aria.groult@ynov.com",
+            "username": "Aria",
+            "password": "azerty"
+        },
+        {
+            "mail": "ronan.robnineau@ynov.com",
+            "username": "Ronan",
+            "password": "ka7rba"
+        }
+    ]
+}
+
+module.exports = Users;
