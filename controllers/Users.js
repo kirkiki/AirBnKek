8e9c4cd025ce78f050a3ed52b3ff5c4ec11c9c89
Aria Groult <aria.groult@outlook.fr>
2017-10-06 15:06:58 +0200

    added login

--- controllers/Users.js
+++ controllers/Users.js
@@ -1,3 +1,4 @@
+const chalk = require('chalk');
 const Users = {
     userList: [{
             "mail": "aria.groult@ynov.com",
@@ -9,7 +10,25 @@ const Users = {
             "username": "Ronan",
             "password": "ka7rba"
         }
-    ]
+    ],
+
+    login: function (req, res, callback) {
+        var username = req.body.username;
+        var password = req.body.password;
+        var loginSuccessfull = false;
+
+        for (let i = 0; i < Users.userList.length; i++) {
+            if (Users.userList[i].username === username) {
+                if (Users.userList[i].password === password) {
+                    loginSuccessfull = true;
+                    res.status(200).redirect('/home');
+                }
+            }
+        }
+        if (loginSuccessfull === false) {
+            console.log(chalk.red.bgWhite('Username or password incorrect'));
+        }
+    }
 }
 
 module.exports = Users;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var Users = require('../controllers/Users.js');
+var chalk = require('chalk');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Express'
+  });
 });
+
+router.post('/login', function (req, res, next) {
+  Users.login(req, res)
+});
+
+router.get('/home', function (req, res, next) {
+  res.render('home');
+})
 
 module.exports = router;
