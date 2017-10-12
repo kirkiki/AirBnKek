a3727c8aeef21c24c065db9cf32d9a60cb460fb1
Aria Groult <aria.groult@outlook.fr>
2017-10-12 09:10:28 +0200

    login done

--- controllers/Users.js
+++ controllers/Users.js
@@ -1,18 +1,20 @@
 const chalk = require('chalk');
 const Users = {
     userList: [{
+            "id": 1,
             "mail": "aria.groult@ynov.com",
             "username": "Aria",
             "password": "azerty"
         },
         {
+            "id": 2,
             "mail": "ronan.robnineau@ynov.com",
             "username": "Ronan",
             "password": "ka7rba"
         }
     ],
 
-    login: function (req, res, callback) {
+    login: function (req, res, next) {
         var username = req.body.username;
         var password = req.body.password;
         var loginSuccessfull = false;
@@ -21,13 +23,23 @@ const Users = {
             if (Users.userList[i].username === username) {
                 if (Users.userList[i].password === password) {
                     loginSuccessfull = true;
-                    res.status(200).redirect('/home');
+                    var newUser = {
+                        username: username,
+                        password: password,
+                        mail: Users.userList[i].mail,
+                        id: Users.userList[i].id
+                    }
+                    res.status(200).redirect('/home/' + Users.userList[i].id);
                 }
             }
         }
         if (loginSuccessfull === false) {
             console.log(chalk.red.bgWhite('Username or password incorrect'));
         }
+    },
+
+    displayUserProfil: function (req, res, next) {
+
     }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var Users = require('../controllers/Users.js');
 var chalk = require('chalk');
 
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', {
@@ -14,8 +15,15 @@ router.post('/login', function (req, res, next) {
   Users.login(req, res)
 });
 
-router.get('/home', function (req, res, next) {
-  res.render('home');
+router.get('/home/:id', function (req, res, next) {
+  res.render('home', {
+    id: req.params.id
+  });
+});
+
+router.get('/profil/:id', function (req, res, next) {
+  var user = Users.displayUserProfil(req, res);
+  next();
 })
 
 module.exports = router;
